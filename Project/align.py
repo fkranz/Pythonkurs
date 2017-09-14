031593f19d48fab16f216c3eea6a0518bf25acb3
Franziska Kranz on Andromeda <franziska.kranz@fau.de>
2017-09-14 12:22:48 +0200

    added final project

---
+++ Project/align.py
@@ -0,0 +1,30 @@
+def fill_matrix(seq1, seq2, S):
+	# compute matrix entries
+
+	return S
+
+def traceback(seq1, seq2, M):
+	# compute alignment from matrix
+
+	return (aseq1, aseq2) #aseq1 and aseq2 are strings
+
+def find_alignment(seq1, seq2): #seq1 and seq2 are strings
+	# setup matrix 
+
+	# fill matrix
+    
+    # compute alignment from matrix
+    return (aseq1, aseq2) # make sure order stays!
+
+
+
+## Main
+s1 = "GCCAGTC"
+s2 = "GACATCA"
+as1,as2 = find_alignment(s1, s2)
+print(as1)
+print(as2)
+
+# expected result
+# GCCAGTC_  
+# GACA_TCA
